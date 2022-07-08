6c57b25f855a946e8d654ada12bc0bfd8a753e6c
Guanglei BAO <toddbgl@hotmail.com>
2022-07-08 17:58:51 +0800

    adding TableDual and Projection

--- ti-explain.js
+++ ti-explain.js
@@ -90,8 +90,11 @@ function explainExplain(explaininfo) {
         let operatorAdv = undefined;
         let operatorURL = undefined;
         switch (operator) {
+            case "TableDual":
+                operatorAdv = "Virtual single row table.";
+                break;
             case "TableFullScan":
-                operatorAdv = "full table scan, consider adding an index.";
+                operatorAdv = "Full table scan, consider adding an index.";
                 break;
             case "TableRangeScan":
                 operatorAdv = "Scans a range of the table.";
@@ -116,6 +119,9 @@ function explainExplain(explaininfo) {
                 break;
             case "HashAgg":
                 operatorURL = "https://docs.pingcap.com/tidb/dev/explain-aggregation#hash-aggregation";
+                break;
+            case "Projection":
+                operatorAdv = "Maps expression value(s) to select list.";
                 break;
             case "StreamAgg":
                 operatorURL = "https://docs.pingcap.com/tidb/dev/explain-aggregation#stream-aggregation";
